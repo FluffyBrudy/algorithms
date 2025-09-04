cd16071a35a8ca58bdcec6a7e916faefe082cd84
rudy <fluffyrudy@proton.me>
2025-09-04 11:59:20 +0545

    add binary-tree-compare.ts

---
+++ binary-tree/binary-tree-compare/binaryTreeComapre.ts
@@ -0,0 +1,46 @@
+/**
+ Given the node object:
+
+Node:
+  val: <int>,
+  left: <Node> or null,
+  right: <Node> or null
+
+write a function compare(a, b) which compares the two trees defined by Nodes a and b and returns true if they are equal in structure and in value and false otherwise.
+
+Examples:
+
+1       1
+| \     | \
+2  3    2  3
+=> true
+
+1       1
+| \     | \
+3  3    2  3
+=> false (values not the same 2 != 3)
+
+1       1
+| \     |
+2  3    2
+        |
+        3
+=> false (structure not the same)
+*/
+export type TNode<T> = {
+  val: T;
+  left: TNode<T> | null;
+  right: TNode<T> | null;
+};
+
+function compare<T>(a: TNode<T> | null, b: TNode<T> | null) {
+  if (a === null && b === null) {
+    return true;
+  }
+  if (a === null || b === null) return false;
+  if (a.val !== b.val) {
+    return false;
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right);
+}
