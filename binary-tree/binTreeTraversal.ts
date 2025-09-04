3d7c80f6a850c8d7ae8d1f8bdafd37602c496b35
rudy <fluffyrudy@proton.me>
2025-09-04 11:20:13 +0545

    remove comment

--- binary-tree/binTreeTraversal.ts
+++ binary-tree/binTreeTraversal.ts
@@ -1,46 +1,45 @@
-import { arrayToTree } from "./arrayToBinaryTree.ts"
+import { arrayToTree } from "./arrayToBinaryTree.ts";
 
-type TNode = ReturnType<typeof arrayToTree>
+type TNode = ReturnType<typeof arrayToTree>;
 
-class BinTreeTraversal<T=any> {
-	private binTree: TNode;
-	constructor(array: Array<T>) {
-		this.binTree = arrayToTree(array);
-	}
+class BinTreeTraversal<T = any> {
+  private binTree: TNode;
+  constructor(array: Array<T>) {
+    this.binTree = arrayToTree(array);
+  }
 
-	printOriginal() {
-		console.log(this.binTree)
-	}
+  printOriginal() {
+    console.log(this.binTree);
+  }
 
-	preOrder(node: TNode=this.binTree) {
-		if (!node) return [];
-		return [
-			node.value, 
-			...this.preOrder(node.left), 
-			...this.preOrder(node.right)
-		]
-	}
+  preOrder(node: TNode = this.binTree) {
+    if (!node) return [];
+    return [
+      node.value,
+      ...this.preOrder(node.left),
+      ...this.preOrder(node.right),
+    ];
+  }
 
-	inOrder(node: TNode=this.binTree) {
-		if (!node) return [];
-		return [
-			...this.inOrder(node.left),
-			node.value,
-			...this.inOrder(node.right)
-		]
-	}
+  inOrder(node: TNode = this.binTree) {
+    if (!node) return [];
+    return [
+      ...this.inOrder(node.left),
+      node.value,
+      ...this.inOrder(node.right),
+    ];
+  }
 
-	postOrder(node: TNode=this.binTree) {
-		if (!node) return [];
-		return [
-			...this.postOrder(node.left),
-			...this.postOrder(node.right),
-			node.value,
-		]
-	}
+  postOrder(node: TNode = this.binTree) {
+    if (!node) return [];
+    return [
+      ...this.postOrder(node.left),
+      ...this.postOrder(node.right),
+      node.value,
+    ];
+  }
 }
 
 const instance = new BinTreeTraversal([1, 2, 3, 4, 5]);
-instance.printOriginal()
-console.log(instance.preOrder())
-// instance.preOrder()
+instance.printOriginal();
+console.log(instance.preOrder());
